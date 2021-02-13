6a1cc4d236b044c6c7169f16ea2ae10d7cc19652
Athilas-Silva <athilas.soares@hotmail.com>
2021-02-13 19:46:39 -0300

    update

---
+++ Aula13/ex13.js
@@ -0,0 +1,23 @@
+//Usando o Do While
+
+// var contador = 1
+// do{
+//     console.log(`Passo ${contador}`)
+//     contador++
+// }
+// while(contador <= 5)
+
+//Usando o While
+
+// var contador = 1
+// while (contador <= 5) {
+//     console.log(`Passo ${contador}`)
+//     contador++
+// }
+
+//Usando o For
+console.log("Vai começar...")
+for(var c = 1; c <= 10; c++){
+    console.log(c)
+}
+console.log("Fim")
---
+++ Aula16/js/script.js
@@ -0,0 +1,33 @@
+function contar(){
+    var inicio = document.getElementById("txti")
+    var fim = document.getElementById("txtf")
+    var passo = document.getElementById("txtp")
+    var res = document.getElementById("res")
+
+    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+        alert("[ERRO] Faltam dados")    
+    }
+    else{
+        res.innerHTML = `Contando: <br>`
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p < 0){
+            alert(`Passo inválido! Considerando o Passo 1`)
+        }
+
+        if(i < f){
+            //Contagem Crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        else{
+            //Contagem Regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
